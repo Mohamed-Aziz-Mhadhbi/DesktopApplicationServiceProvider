22393e932eacf9598cf654764c31cacd66717fbc
azzabihamza <hamza.azzabi@esprit.tn>
2021-04-10 19:29:29 +0100

    add user files

---
+++ src/Interfaces/InterfaceUser.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+import Entities.User;
+import java.util.List;
+
+
+public interface InterfaceUser {
+    public void addUser(User user);
+    public List<User> displayUsers();
+
+}
--- src/Services/ServiceUser.java
+++ src/Services/ServiceUser.java
@@ -1,4 +1,18 @@
 package Services;
 
-public class ServiceUser {
+import Entities.User;
+import Interfaces.InterfaceUser;
+
+import java.util.List;
+
+public class ServiceUser implements InterfaceUser {
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public List<User> displayUsers() {
+        return null;
+    }
 }
